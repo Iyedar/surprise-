c7a5ffb797c3f75a4fa32f4f0db8a096851d4ebf
Iyedar <123387088+Iyedar@users.noreply.github.com>
2025-02-10 22:14:18 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I KNOW YOU LOVE FLOWERS YA OMRI').split('')
+    const titles = ('I KNOW YOU LOVE FLOWERS YA ABIR').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
